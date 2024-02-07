d71ee27d0cc445f22b181d4800303a6bfb9e3815
Sara Lafleur <sara.m.lafleur@gmail.com>
2024-02-07 14:54:38 +0100

    Setup the application

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
-  Hello World
+  <div id="app">
+    <header>
+      <h1>header here</h1>
+    </header>
+    <main>
+      <h2>cocktail gallery here</h2>
+      <CocktailGallery />
+    </main>
+    <footer>
+      <p>footer here</p>
+    </footer>
+  </div>
 </template>
 
 <script>
-export default {}
+  import CocktailGallery from '@/components/CocktailGallery.vue'
+  
+  export default {
+    name: 'App',
+    components: {
+      CocktailGallery
+    }
+  }
 </script>
