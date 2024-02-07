24557c94d91596f020ac4e1c53924038306134cd
Sara Lafleur <sara.m.lafleur@gmail.com>
2024-02-07 14:52:22 +0100

    Fetch the API's data

---
+++ src/services/api/cocktailAPI.js
@@ -0,0 +1,10 @@
+const getCocktailData = async function() {
+    const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a")
+    if (response.status == 200) {
+        return await response.json()
+    } else {
+        new Error(response.statusText)
+    }
+}
+
+export default { getCocktailData }
