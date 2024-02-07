b03e3f7aad0a54765a74ac134c748aa020bc5489
Sara Lafleur <sara.m.lafleur@gmail.com>
2024-02-07 14:54:00 +0100

    Add Gallery component

---
+++ src/components/CocktailGallery.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="cocktail-gallery">
+        <CocktailCard
+            v-for="cocktail in cocktails"
+            :strDrinkThumb="cocktail.strDrinkThumb"
+            :strDrink="cocktail.strDrink"
+            :strCategory="cocktail.strCategory"
+            :strGlass="cocktail.strGlass"
+            :strAlcoholic="cocktail.strAlcoholic" />
+    </div>
+</template>
+
+<script>
+    import CocktailData from '@/services/api/cocktailAPI.js'
+    import CocktailCard from '@/components/CocktailCard.vue'
+
+    export default {
+        name: 'CocktailGallery',
+        components: {
+            CocktailCard
+        },
+        data() {
+            return {
+                cocktails: []
+            }
+        },
+        created: async function() {
+            this.retrieveCocktailData()
+        },
+        methods: {
+            async retrieveCocktailData() {
+                this.cocktails = await CocktailData.getCocktailData()
+            }
+        }
+    }
+</script>
+
+<style scoped></style>
