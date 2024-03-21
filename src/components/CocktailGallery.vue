1b6d2f2e9f04d777fc8aaef786d65ea6aad5ac7e
Sara Lafleur <sara.m.lafleur@gmail.com>
2024-03-21 15:43:10 +0100

    Add alcoholic - non alcoholic filter

--- src/components/CocktailGallery.vue
+++ src/components/CocktailGallery.vue
@@ -4,7 +4,6 @@
             <input type="text" v-model="search" placeholder="Search for a Cocktail...">
             <button v-if="search" v-on:click="applyFilters">search</button>
             <button v-if="search" v-on:click="cleanSearch">clear</button>
-            <button v-on:click="retrieveCocktailData">reset</button>
 
             <label for="cocktail-sort">Sort by: </label>
             <select v-model="cocktailSortType" id="cocktail-sort">
@@ -15,7 +14,15 @@
                 <option name="AZGlass" value="AZGlass">Glass A -> Z</option>
                 <option name="ZAGlass" value="ZAGlass">Glass Z -> A</option>
             </select>
+
+            <input type="checkbox" v-model="aIsChecked" name="alcoholic" value="alcoholic">
+            <label for="alcoholicOrNot">alcoholic</label>
+            <input type="checkbox" v-model="NaIsChecked" name="non-alcoholic" value="non-alcoholic">
+            <label for="alcoholicOrNot">non-alcoholic</label>
+
             <button v-on:click="applyFilters">filter</button>
+
+            <button v-on:click="retrieveCocktailData">reset all</button>
         </div>
 
         <div class="gallery-display">
@@ -41,6 +48,23 @@
         },
         computed: {
             organizedCocktailData: function() {
+                // search
+                const filterSearch = (a) => a.strDrink.toLowerCase().includes(this.search.toLowerCase())
+
+                // alcoholic or not
+                const filterAlcoholic = (a) => {
+                    if (this.aIsChecked && this.NaIsChecked) {
+                        return a.strAlcoholic.toLowerCase().includes("a") // Show all
+                    } else if (this.aIsChecked) {
+                        return !a.strAlcoholic.toLowerCase().includes("no")
+                    } else if (this.NaIsChecked) {
+                        return a.strAlcoholic.toLowerCase().includes("no")
+                    } else {
+                        return a.strAlcoholic.toLowerCase().includes("a") // Show all
+                    }
+                }
+
+                // sort
                 let field = ""
                 if (["AZName", "ZAName"].includes(this.cocktailSortType)) {
                     field = "strDrink"
@@ -51,19 +75,24 @@
                 } else {
                     console.error("Unexpected value for cocktailSortType: ", this.cocktailSortType);
                 }
-                console.log("field = ", field)
                 let reversed = ["ZAName", "ZACategory", "ZAGlass"].includes(this.cocktailSortType) ? -1 : 1
-                console.log("reversed = ", reversed)
-                return this.cocktails
-                    .filter((a) => a.strDrink.toLowerCase().includes(this.search.toLowerCase()))
-                    .sort((a, b) => a[field].localeCompare(b[field]) * reversed)
+                const comparator = (a, b) => a[field].localeCompare(b[field]) * reversed
+
+                const data = [...this.cocktails]
+                
+                return data
+                    .filter(filterSearch)
+                    .filter(filterAlcoholic)
+                    .sort(comparator)
             }
         },
         data() {
             return {
                 cocktails: [],
                 search: "",
-                cocktailSortType: "AZName"
+                cocktailSortType: "AZName",
+                aIsChecked: "",
+                aIsNotChecked: ""
             }
         },
         created: async function() {
@@ -73,6 +102,8 @@
             async retrieveCocktailData() {
                 this.cocktails = await CocktailData.getCocktailData()
                 this.cleanSearch()
+                this.aIsChecked = false
+                this.NaIsChecked = false
                 this.applyFilters()
             },
             cleanSearch: function() {
@@ -86,6 +117,15 @@
 </script>
 
 <style scoped>
+    .gallery-options {
+        background-color: rgb(197, 255, 110);
+    }
+    input {
+        margin: 10px;
+    }
+    button {
+        margin: 10px;
+    }
     .gallery-display {
         display: grid;
         grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
